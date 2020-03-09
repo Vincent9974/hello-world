2de765bb6915abbe8ac45d84e6392d40980215b4
Vincent9974 <50794481+Vincent9974@users.noreply.github.com>
2020-03-09 15:48:14 +0800

    Create String.java
    随机找了一个程序

---
+++ String.java
@@ -0,0 +1,18 @@
+public class ArraysDemo {
+
+    //把数组中的元素按照指定的规则组成一个字符串：[元素1, 元素2, ...]
+    public static String arrayToString(int[] arr) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        for (int i = 0; i < arr.length; i++) {
+            if (i == arr.length - 1) {
+                sb.append(arr[i]);
+            } else {
+                sb.append(arr[i]).append(", ");
+            }
+        }
+        sb.append("]");
+        String s = sb.toString();
+        return s;
+    }
+}
